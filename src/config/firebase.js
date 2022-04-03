094ad686755165006759d10a7b5c115e22e99030
Michelsom <michelsonpagani@gmail.com>
2022-04-03 09:21:25 -0400

    feat:add firebase

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+// import {initializeApp} from '@react-native-firebase/app';
+// import {getAnalytics} from 'firebase/analytics';
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBj2Q2Ey9v2fqJldu82mLDNcUWKrouLZqk',
+//   authDomain: 'develcode-e82e5.firebaseapp.com',
+//   // projectId: 'develcode-e82e5',
+//   storageBucket: 'develcode-e82e5.appspot.com',
+//   messagingSenderId: '932694122991',
+//   appId: '1:932694122991:web:e4fcd951da02450fc4161a',
+//   measurementId: 'G-WKKXBK7D8S',
+// };
+
+// // Initialize Firebase
+// export const database = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(database);
