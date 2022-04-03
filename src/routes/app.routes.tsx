a5c01306b6a851fcf5591f5994bc314a25532f82
Michelsom <michelsonpagani@gmail.com>
2022-04-03 09:25:31 -0400

    routes: add new routes

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,15 +1,22 @@
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-// import { Home } from '../screens/Home';
-
+import {Home} from '../screens/Home';
+import { Profile } from '../screens/Profile';
+import {Register} from '../screens/Register';
+import {Splash} from '../screens/Splash';
 
 const Stack = createNativeStackNavigator();
 
 export const AppRoutes = () => {
   return (
-    <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
-      {/* <Stack.Screen name="Home" component={Home} /> */}
-     
+    <Stack.Navigator
+      initialRouteName="Splash"
+      screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Splash" component={Splash} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="Profile" component={Profile} />
+
     </Stack.Navigator>
   );
 };
