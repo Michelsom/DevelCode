c531a9beadf6668dc59a7a57f7edba2ecf5a2b00
Michelsom <michelsonpagani@gmail.com>
2022-04-03 09:14:42 -0400

    feat:create new global button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import {ActivityIndicator} from 'react-native';
+import theme from '../../global/styles/theme';
+import * as S from './styles';
+
+interface ButtonProps {
+  text: string;
+  type: 'solid' | 'outline' | 'transparent';
+  color:
+    | 'primaryColor'
+    | 'secondaryColor'
+    | 'disabledColor'
+    | 'dangerColor'
+    | 'defaultColor';
+  loading?: boolean;
+  gradient?: boolean;
+  disable?: boolean;
+  width?: number;
+  transparent?: boolean;
+  handleButton: () => void;
+}
+
+const colors = [
+  {id: 1, name: 'primaryColor', color: theme.colors.primaryColor},
+  {id: 2, name: 'secondaryColor', color: theme.colors.secondaryColor},
+  {id: 3, name: 'disabledColor', color: theme.colors.disableColor},
+  {id: 4, name: 'dangerColor', color: theme.colors.error},
+  {id: 5, name: 'defaultColor', color: theme.colors.defaultTextDescription},
+];
+
+export const GlobalButton = ({
+  color,
+  text,
+  type,
+  gradient,
+  loading,
+  width,
+  handleButton,
+  disable,
+  transparent,
+}: ButtonProps) => {
+  return (
+    <S.Button
+      transparent={transparent}
+      disable={disable}
+      width={width}
+      color={color}
+      type={type}
+      onPress={() => {
+        if (!disable || loading) {
+          handleButton();
+        }
+      }}>
+      {loading ? (
+        <ActivityIndicator
+          color={
+            type === 'solid'
+              ? theme.colors.lightColor
+              : colors.find(e => e.name === color)?.color
+          }
+        />
+      ) : (
+        <S.TextButton disable={disable} color={color} type={type}>
+          {text}
+        </S.TextButton>
+      )}
+    </S.Button>
+  );
+};
